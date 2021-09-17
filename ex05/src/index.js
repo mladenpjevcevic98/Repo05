7c3ef58eeba1729d16f07e148507953a3721e734
mladenpjevcevic98 <pjevcevicm237@gmail.com>
2021-09-17 17:51:42 +0200

    Concatenating strings with the plus equals operator

---
+++ ex05/src/index.js
@@ -0,0 +1,11 @@
+function main()
+{
+    var myString = "Concatentation is the process of appending ";
+
+    myString += "one string to the end of another string.";
+
+    return myString
+}
+
+console.log(main());
+module.exports = main;
