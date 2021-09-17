d0429252bc953fa056ca0826353c2abe9f7b702d
mladenpjevcevic98 <pjevcevicm237@gmail.com>
2021-09-17 17:57:28 +0200

    Use bracket notation to find the last character in a string

---
+++ ex12/src/index.js
@@ -0,0 +1,10 @@
+function main()
+{
+    var lastName = "Tesla";
+    var lastLetterofLastName = lastName[lastName.length - 1];
+
+    return lastLetterofLastName;
+}
+
+console.log(main());
+module.exports = main;
