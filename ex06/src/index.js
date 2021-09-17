b228e78a2a5d64430d2166935b8a4a12bc5dc826
mladenpjevcevic98 <pjevcevicm237@gmail.com>
2021-09-17 17:52:26 +0200

    Constructing strings with variables

---
+++ ex06/src/index.js
@@ -0,0 +1,11 @@
+function main()
+{
+    var myName = "Mladen";
+    var myString = "My name is " + myName + " and I am well.";
+
+    return myString;
+}
+
+console.log(main());
+
+module.exports = main;
