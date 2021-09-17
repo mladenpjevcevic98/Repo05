23e5b7fa13457e3ba6598ef382a2cfbca2fa61f3
mladenpjevcevic98 <pjevcevicm237@gmail.com>
2021-09-17 17:58:45 +0200

    Word blanks

---
+++ ex14/src/index.js
@@ -0,0 +1,15 @@
+function main()
+{
+
+    var myNoun = "Dog ";
+    var myVerb = "ran ";
+    var myAdjective = "big ";
+    var myAdverb = "quickly ";
+
+    var wordBlanks = myNoun + myVerb + myAdverb;
+
+    return wordBlanks;
+}
+console.log(main());
+
+module.exports = main;
